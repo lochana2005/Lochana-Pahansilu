ba1b42a0b6cbc6b7532e2007414272de9525a35c
Lochana Pahansilu <123851797+lochana2005@users.noreply.github.com>
2024-04-19 01:34:51 +0530

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>About Us</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="content row d-flex align-content-center" style="width: 100%; height: 60vh;">
+                <h1 class="text-center ha">About Us</h1>
+                <p class="text-center myp mt-5">
+                    Lorem ipsum is a placeholder text used in publishing and graphic
+                    design. It's often used to show the visual form of a document or a
+                    typeface without using meaningful content. Lorem ipsum can also be
+                    used as a placeholder before the final copy is available.Lorem ipsum
+                    is a placeholder text used in publishing and graphic design. It's
+                    often used to show the visual form of a document or a typeface
+                    without using meaningful content. Lorem ipsum can also be used as a
+                    placeholder before the final copy is available. Lorem ipsum is a
+                    placeholder text used in publishing and graphic design. It's often
+                    used to show the visual form of a document or a typeface without
+                    using meaningful content. Lorem ipsum can also be used as a
+                    placeholder before the final copy is available.
+                </p>
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-end">
+                        <button class="bn bn1"><i class="fa-brands fa-facebook"></i> FaceBook</button>
+
+                    </div>
+                    <div class="col-6">
+                        <button class="bn bn2"><i class="fa-brands fa-whatsapp"></i><span class="ms-1">Whatsapp</span>  </button>
+
+                    </div>
+                    
+                    
+                </div>
+               <div class="d-flex justify-content-center">
+                <button class="bne mt-4 me-3">Explore More</button>
+               </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
